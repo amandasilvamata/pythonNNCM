03e202e69f340983d5cd465d3bc4915e64adc756
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2025-09-18 20:55:57 -0300

    Create arquivo5.py

---
+++ variaveis e entradas/arquivo5.py
@@ -0,0 +1,9 @@
+# --- tempo de viagem ---
+
+S = float(input("Digite a distância do trajeto: "))
+
+Vm = float(input("Digite a velocidade média: "))
+
+t = S / Vm
+
+print ("Em uma distância de %.2f km com a velocidade média de %.2f km/h você chegará ao seu destino em %.1fh" %(S, Vm, t))
