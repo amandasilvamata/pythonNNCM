60141d953a69cd52e605f73e0a927ed31fa7e976
amandasilvamata <166837389+amandasilvamata@users.noreply.github.com>
2025-09-18 21:03:03 -0300

    Create arquivo6.py

---
+++ variaveis e entradas/arquivo6.py
@@ -0,0 +1,7 @@
+# --- conversão de temperatura ---
+
+C = float(input("Digite a temperatura em Celsius: "))
+
+F = (9 * C / 5) + 32
+
+print("A temperatura de %.2f°C em Celsius é de %.2f°F" %(C, F))
